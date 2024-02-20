7ac984cd0f4d16da280214dbe04397458979090f
Akaczek <luk12349876@o2.pl>
2024-02-20 13:31:21 +0100

    Layout ready for implementing targets

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='aim-trainer'>
+      <h1 className='aim-trainer__title'>Aim Trainer</h1>
+      <div className='aim-trainer__train-box'>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
