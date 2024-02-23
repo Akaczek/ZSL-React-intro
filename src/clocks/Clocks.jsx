583eabd3c1538c0427f8f64e1e25ab71551acdfa
Akaczek <luk12349876@o2.pl>
2024-02-23 13:45:39 +0100

    Clocks second checkpoint

--- src/clocks/Clocks.jsx
+++ src/clocks/Clocks.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from 'react';
 import './Clocks.css';
 
 const Clocks = () => {
+  const d = new Date();
+  const [seconds, setSeconds] = useState(d.getSeconds());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nd = new Date();
+      setSeconds(nd.getSeconds());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [])
+
   return (
     <div className='clocks'>
       <div className='clocks__single-clock-wrapper'>
         <div className='clocks__single-clock'>
-          <div className='clock-hand'>
+          <div className='clock-hand' style={{
+            transform: `rotate(${seconds * 6}deg)`
+          }}>
             <div className='black-part'></div>
             <div className='white-part'></div>
           </div>
